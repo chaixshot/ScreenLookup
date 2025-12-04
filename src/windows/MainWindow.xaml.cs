5d6f6474df4b2cfeb8d81e93a5d7eb031a8921db
chaixshot <chai5142xshot@gmail.com>
2025-12-04 23:21:52 +0700

    feat: TrayIcon

--- src/windows/MainWindow.xaml.cs
+++ src/windows/MainWindow.xaml.cs
@@ -2,8 +2,15 @@
 using HotkeyUtility.Input;  // Contains HotkeyEventArgs
 using ScreenTranslator.src.windows;
 using System.Diagnostics;
+using System.Drawing;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
+using System.Windows.Media.Media3D;
+using Wpf.Ui.Appearance;
+using Wpf.Ui.Controls;
+using Wpf.Ui.Tray.Controls;
+using NotifyIcon = System.Windows.Forms.NotifyIcon;
 
 namespace ScreenTranslator
 {
@@ -13,11 +20,12 @@ namespace ScreenTranslator
     public partial class MainWindow : Window
     {
         public CaptureWindow? CaptureWindow { get; set; } = null;
-
+        public NotifyIcon notifyIcon = new NotifyIcon();
 
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowHide();
 
             // Added hotkey
             Hotkey hotkey = new(Key.A, ModifierKeys.Shift | ModifierKeys.Control, Button_Capture);
@@ -29,16 +37,59 @@ namespace ScreenTranslator
             // Instead, Hotkey_Pressed will now be triggered by pressing Alt + B.
             _ = hotkeyManager.TryReplaceHotkey(Key.A, ModifierKeys.Shift | ModifierKeys.Control, Key.Z, ModifierKeys.Alt);
 
+            //Tray
+            notifyIcon.Icon = new Icon(@"..\..\..\src\logo.ico");
+            notifyIcon.Visible = true;
+            notifyIcon.ShowBalloonTip(1000, "ScreenLookup", "ScreenLookup started in the background", ToolTipIcon.Info);
+
+            notifyIcon.MouseClick += NotifyIcon_Click;
+
+            notifyIcon.ContextMenuStrip = new ContextMenuStrip();
+            notifyIcon.ContextMenuStrip.Items.Add("Open", new Bitmap(1, 1), (s, e) =>
+            {
+                MainWindowShow();
+            });
+            notifyIcon.ContextMenuStrip.Items.Add("Capture              Alt+Z", new Bitmap(1, 1), Button_Capture);
+            notifyIcon.ContextMenuStrip.Items.Add("Quit", new Bitmap(1, 1), (s, e) =>
+            {
+                MainWindowHide();
+            });
         }
 
-        private void Button_Capture(object sender, RoutedEventArgs e)
+        private void Button_Capture(object sender, EventArgs e)
         {
             CaptureWindow = new CaptureWindow();
             CaptureWindow.Show();
         }
 
-        private void Button_Tray(object sender, RoutedEventArgs e)
+        private void WindowStateChanged(object sender, EventArgs e)
         {
+            if (WindowState == (WindowState)FormWindowState.Minimized)
+            {
+                MainWindowHide();
+            }
+        }
+
+        private void NotifyIcon_Click(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                MainWindowShow();
+            }
+
+        }
+
+        private void MainWindowShow()
+        {
+
+            this.Show();
+            WindowState = (WindowState)FormWindowState.Normal;
+        }
+
+        private void MainWindowHide()
+        {
+
+            this.Hide();
         }
     }
 }
