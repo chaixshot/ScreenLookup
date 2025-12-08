50c2849fd853ae6e1d5b90504456594af54de370
chaixshot <chai5142xshot@gmail.com>
2025-12-08 21:04:51 +0700

    improved Convertor

--- src/utils/Convertor.cs
+++ src/utils/Convertor.cs
@@ -6,20 +6,60 @@ namespace ScreenLookup.src.utils
     class Convertor
     {
         public static MainWindow mainWindow = (App.Current.MainWindow as MainWindow);
-        public static List<CaptureWordsEntry> ConvertCaptureWordsEntry(List<CaptureWordsEntrySimplify> data)
+        public static List<CaptureWordsEntry> ConvertCaptureWordsEntry(List<CaptureWordsEntrySimplify> data, int sourceLanguage = 0, int targetLanguage = 0)
         {
             List<CaptureWordsEntry> itemsForCard = [];
 
             foreach (var item in data)
             {
                 if (item.Stop == 0)
-                    itemsForCard.Add(new CaptureWordsEntry() { Word = item.Word, Border = "1", FontSizeS = Setting.FontSizeS, FontFace = new FontFamily(Setting.FontFace) });
+                    itemsForCard.Add(new CaptureWordsEntry()
+                    {
+                        Word = item.Word,
+                        Width = Double.NaN,
+                        Height = Double.NaN,
+                        Border = 1,
+                        FontSizeS = Setting.FontSizeS,
+                        FontFace = new FontFamily(Setting.FontFace),
+                        SourceLanguage = sourceLanguage,
+                        TargetLanguage = targetLanguage
+                    });
                 if (item.Stop == 1)
-                    itemsForCard.Add(new CaptureWordsEntry() { Word = "", Width = mainWindow.Width.ToString(), Height = "0" });
+                    itemsForCard.Add(new CaptureWordsEntry()
+                    {
+                        Word = "",
+                        Width = mainWindow.Width,
+                        Height = 0,
+                        Border = 0,
+                        FontSizeS = Setting.FontSizeS,
+                        FontFace = new FontFamily(Setting.FontFace),
+                        SourceLanguage = 0,
+                        TargetLanguage = 0
+                    });
                 if (item.Stop == 2)
-                    itemsForCard.Add(new CaptureWordsEntry() { Word = "", Width = mainWindow.Width.ToString() });
+                    itemsForCard.Add(new CaptureWordsEntry()
+                    {
+                        Word = "",
+                        Width = mainWindow.Width,
+                        Height = Double.NaN,
+                        Border = 0,
+                        FontSizeS = Setting.FontSizeS,
+                        FontFace = new FontFamily(Setting.FontFace),
+                        SourceLanguage = 0,
+                        TargetLanguage = 0
+                    });
                 if (item.Stop == 3)
-                    itemsForCard.Add(new CaptureWordsEntry() { Word = "", Width = mainWindow.Width.ToString() });
+                    itemsForCard.Add(new CaptureWordsEntry()
+                    {
+                        Word = "",
+                        Width = mainWindow.Width,
+                        Height = Double.NaN,
+                        Border = 0,
+                        FontSizeS = Setting.FontSizeS,
+                        FontFace = new FontFamily(Setting.FontFace),
+                        SourceLanguage = 0,
+                        TargetLanguage = 0
+                    });
             }
 
             return itemsForCard;
