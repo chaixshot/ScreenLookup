efe136057ebfdc91e486672193c19e151c00d52a
chaixshot <chai5142xshot@gmail.com>
2025-12-10 21:42:54 +0700

    SnackbarHost fixed snackbar selector

--- src/utils/SnackbarHost.cs
+++ src/utils/SnackbarHost.cs
@@ -4,14 +4,15 @@ namespace ScreenLookup.src.utils
 {
     internal class SnackbarHost
     {
-        public static Snackbar? snackbar;
+        public static Snackbar? snackbarMain;
+        public static Snackbar? snackbarCapture;
         public static MainWindow? mainWindow = (MainWindow)App.Current.MainWindow;
-        public static SnackbarPresenter? snackbarMain = mainWindow?.snackbarHost;
 
         public static void Show(string title = "", string message = "", string type = "info", int timeout = 5, int width = 500, string windows = "main", bool closeButton = true)
         {
             ControlAppearance appearance;
             SymbolIcon icon;
+            Snackbar? snackbar;
 
             if (type == "warning")
             {
@@ -35,9 +36,15 @@ namespace ScreenLookup.src.utils
             }
 
             if (windows == "main")
-                snackbar ??= new Snackbar(snackbarMain);
+            {
+                snackbarMain ??= new Snackbar(mainWindow?.snackbarHost);
+                snackbar = snackbarMain;
+            }
             else
-                snackbar ??= new Snackbar((mainWindow?.GetCaptureWindow().snackbarHost));
+            {
+                snackbarCapture ??= new Snackbar((mainWindow?.GetCaptureWindow().snackbarHost));
+                snackbar = snackbarCapture;
+            }
 
             snackbar.SetCurrentValue(Snackbar.TitleProperty, title);
             snackbar.SetCurrentValue(System.Windows.Controls.ContentControl.ContentProperty, message);
