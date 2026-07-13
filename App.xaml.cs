c857338b32a72fd1cd754971b6581f77c4f18976
chaixshot <chai5142xshot@gmail.com>
2026-07-13 14:40:30 +0700

    added: Handle for explorer.exe restart to restore tray icon

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,14 +4,20 @@ using ScreenLookup.src.pages;
 using ScreenLookup.src.utils;
 using ScreenLookup.src.windows;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 using Wpf.Ui.Controls;
 
 namespace ScreenLookup
 {
     public partial class App : Application
     {
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern uint RegisterWindowMessage(string lpString);
+        private static uint taskbarCreatedMessage;
+
         public static readonly string tempFolder = Path.Combine(Path.GetTempPath(), "ScreenLookup");
         public static readonly string appDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ScreenLookup");
 
@@ -48,7 +54,35 @@ namespace ScreenLookup
 
             ToggleTopmost();
 
+            // Handle for explorer.exe restart
+            {
+                taskbarCreatedMessage = RegisterWindowMessage("TaskbarCreated"); // Register the message and hook into the MainWindow's message pump
+
+                // MainWindow handle to listen for the OS broadcast
+                var wih = new WindowInteropHelper(mainWindow);
+                wih.EnsureHandle(); // Ensures the HWND exists even if hidden
+
+                HwndSource source = HwndSource.FromHwnd(wih.Handle);
+                source.AddHook(HwndMessageHook);
+            }
+
             base.OnStartup(e);
+        }
+
+        // The message loop handler
+        private static IntPtr HwndMessageHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // Check if the message matches the "TaskbarCreated" ID assigned by Windows
+            if (msg == taskbarCreatedMessage)
+            {
+                trayIcon?.Close();
+                trayIcon = new TrayIcon();
+                trayIcon.Show();
+
+                SetupHoykey(); // Refresh the shortcut string on the new tray item header
+            }
+
+            return IntPtr.Zero;
         }
 
         public static void ToggleTopmost(bool? enabled = null)
@@ -71,7 +105,6 @@ namespace ScreenLookup
 
         public static void SetupHoykey()
         {
-
             ShortcutKeySet shortcutKey = setting.ShortcutKey;
             ModifierKeys modifierKey = ModifierKeys.None;
             trayIcon.trayCapture.Header = "Lookup".PadRight(20);
